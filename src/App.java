fb17770f9387e3cbe86d574aee88b2bbb47b7aca
Alexandre de Oliveira <alexmontanha@hotmail.com>
2022-10-07 20:44:55 -0300

    Ajuste para imprimir o número da coluna

--- src/App.java
+++ src/App.java
@@ -15,7 +15,7 @@ public class App {
     public static void imprimirLinhas(int n) {
         for (int i = 1; i <= n; i++) {
            for (int j = 1; j <= i; j++) {
-                System.out.print(i + "  "); 
+                System.out.print(j + "  "); 
            }
            System.out.println(" ");
         }
