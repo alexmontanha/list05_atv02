ea09014d6851b6dacf4451571d825913792ff194
Alexandre de Oliveira <alexmontanha@hotmail.com>
2022-10-07 20:43:19 -0300

    Função já imprime colunas

--- src/App.java
+++ src/App.java
@@ -14,7 +14,10 @@ public class App {
 
     public static void imprimirLinhas(int n) {
         for (int i = 1; i <= n; i++) {
-           System.out.println(i);
+           for (int j = 1; j <= i; j++) {
+                System.out.print(i + "  "); 
+           }
+           System.out.println(" ");
         }
     }
 }
