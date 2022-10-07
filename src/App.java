dafa7508e16cd8fe60c2755f083dac66751ec82f
Alexandre de Oliveira <alexmontanha@hotmail.com>
2022-10-07 20:34:14 -0300

    Lendo o número de linhas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Entre com o número de linhas");
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        sc.close();
     }
 }
